4b04e9edc6c471dbb32b1d61eff5c2c2daf46ba0
Jae S Choi <choi.jae.seok@gmail.com>
2019-10-23 13:56:44 -0300

    reorg param calls

--- R/speciescomposition_parameters.R
+++ R/speciescomposition_parameters.R
@@ -94,7 +94,7 @@ speciescomposition_parameters = function( p=NULL, project_name=NULL, project_cla
 
     if ( !exists("project_name", p)) p$project_name = "speciescomposition"
 
-    p = aegis_parameters( p=p, DS="carstm" )  #generics
+    p = carstm_parameters( p=p, DS="basic" )  #generics
 
     if ( !exists("areal_units_strata_type", p)) p$areal_units_strata_type = "lattice" # "stmv_lattice" to use ageis fields instead of carstm fields ... note variables are not the same
 
