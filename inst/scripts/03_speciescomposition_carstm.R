afa62f01b9cc2eb282d434ef4448027d77500daf
Jae S Choi <choi.jae.seok@gmail.com>
2021-03-09 20:08:51 -0400

    tweaking

--- inst/scripts/03_speciescomposition_carstm.R
+++ inst/scripts/03_speciescomposition_carstm.R
@@ -98,7 +98,7 @@ for ( variabletomodel in c("pca1", "pca2"))  {
     plot_crs = p$aegis_proj4string_planar_km
 
     coastline=aegis.coastline::coastline_db( DS="eastcoast_gadm", project_to=plot_crs )
-    isobaths=aegis.bathymetry::isobath_db( depths=c(50, 100, 200, 400, 800), project_to=plot_crs  )
+    isobaths=aegis.bathymetry::isobath_db( depths=c(50, 100, 200, 400 ), project_to=plot_crs  )
 
  
     vn = paste( variabletomodel, "predicted", sep=".")
