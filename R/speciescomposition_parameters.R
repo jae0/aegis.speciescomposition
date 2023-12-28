1dccf5e3ba39ef0a6e7b139da654a76f20367a15
Jae S Choi <choijaeseok@gmail.com>
2023-12-28 18:41:44 -0400

    complete mods for 2023

--- R/speciescomposition_parameters.R
+++ R/speciescomposition_parameters.R
@@ -153,8 +153,13 @@ speciescomposition_parameters = function( p=list(), project_name="speciescomposi
       fraction_todrop = 0.05,
       fraction_cv = 0.9,
       fraction_good_bad = 1.0,
-      areal_units_constraint_nmin=5,  # best compromise
-      areal_units_constraint_ntarget =  floor(length(p$yrs)/2),  # n time slices req in each au
+      areal_units_constraint_ntarget=10, 
+      areal_units_constraint_nmin=1,  # granularity options for areal_units
+      # areal_units_constraint_ntarget =  floor(length(p$yrs)/2),  # n time slices req in each au
+      # areal_units_constraint_nmin=5,  # best compromise
+      spbuffer=5, 
+      lenprob=0.95,   # these are domain boundary options for areal_units
+      n_iter_drop=0, sa_threshold_km2=16, 
       nAU_min = 50,
       carstm_modelengine = "inla",  # {model engine}.{label to use to store}
       carstm_model_label = "1999_present",
