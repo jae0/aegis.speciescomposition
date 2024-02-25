6be349419e641ba1b15416fbb80687a6dee35246
Jae S Choi <choijaeseok@gmail.com>
2024-02-25 14:16:19 -0400

    tidy

--- inst/scripts/01_speciescomposition_carstm_1999_to_present.R
+++ inst/scripts/01_speciescomposition_carstm_1999_to_present.R
@@ -298,6 +298,38 @@ for ( variabletomodel in c("pca1", "pca2", "pca3")) { #  , "pca3" , "ca1", "ca2"
         outfilename=outfilename
     )
  
+
+    # posterior predictive check
+    
+    MM = speciescomposition_db( p=p, DS='carstm_inputs' )
+    iobs = which(MM$tag == "observations")
+    vn = variabletomodel
+
+    fit = NULL; gc()
+    fit = carstm_model( p=p, DS="carstm_modelled_fit") #,  sppoly = sppoly )
+
+    pld = data.table(
+      observed = MM[iobs , ..vn] , 
+      fitted = fit$summary.fitted.values[["mean"]] [iobs]
+    )
+    names(pld) = c("observed", "fitted")
+    anno1 = paste( "Pearson correlation: ", round( cor( pld$fitted, pld$observed, use="pairwise.complete.obs" ), 3))
+    # cor( fitted, observed, use="pairwise.complete.obs", "spearman" )
+
+    out = ggplot(pld, aes(x =  observed, y = fitted )) +
+      geom_abline(slope=1, intercept=0, color="darkgray", lwd=1.4 ) +
+      geom_point(color="slategray") +
+      labs(caption=anno1, color="slateblue") +
+      theme( plot.caption = element_text(hjust = 0, size=12 ) )# move caption to the left 
+   
+    outputdir = file.path( p$modeldir, p$carstm_model_label )
+    fn = file.path(outputdir, paste("posterior_predictive_check_", vn, ".png", sep="") )
+    ggsave(filename=fn, plot=out, device="png", width=12, height = 8)
+    print(out)    
+
+    fit  = MM = NULL; gc()
+
+
   }
 
 
